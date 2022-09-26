461450317fe6a6070e51a440cf3ba7bdd6241709
Langerz82 <langley.joshua@gmail.com>
2022-09-26 22:40:22 +1000

    kill advmame test.

--- gptokeyb.cpp
+++ gptokeyb.cpp
@@ -103,6 +103,7 @@ std::vector<config_option> parseConfigFile(const char* path)
 static int uinp_fd = -1;
 struct uinput_user_dev uidev;
 
+bool kill_advmame = false;
 bool kill_mode = false;
 bool sudo_kill = false; //allow sudo kill instead of killall for non-emuelec systems
 bool pckill_mode = false; //emit alt+f4 to close apps on pc during kill mode, if env variable is set
@@ -1180,6 +1181,26 @@ void emit(int type, int code, int val)
   write(uinp_fd, &ev, sizeof(ev));
 }
 
+void emitButton(int code, bool is_pressed, int modifier = 0)
+{
+  if (!(modifier == 0) && is_pressed) {
+    emit(EV_SYN, modifier, is_pressed ? 1 : 0);
+    emit(EV_SYN, SYN_REPORT, 0);
+  }
+  emit(EV_SYN, code, is_pressed ? 1 : 0);
+  emit(EV_SYN, SYN_REPORT, 0);
+  if (!(modifier == 0) && !(is_pressed)) {
+    emit(EV_SYN, modifier, is_pressed ? 1 : 0);
+    emit(EV_SYN, SYN_REPORT, 0);
+  }
+}
+
+void pressButton(int code, int duration = 50) {
+  emitButton(code,true,0);
+  SDL_Delay(duration);
+  emitButton(code,false,0);  
+}
+
 void emitKey(int code, bool is_pressed, int modifier = 0)
 {
   if (!(modifier == 0) && is_pressed) {
@@ -1634,7 +1655,16 @@ SDL_GameController* controller = SDL_GameControllerFromInstanceID(event.cdevice.
             emitAxisMotion(ABS_HAT0X, is_pressed ? 1 : 0);
             break;
         }
-         if ((kill_mode) && (state.start_pressed && state.hotkey_pressed)) {      
+         if ((kill_mode) && (state.start_pressed && state.hotkey_pressed)) {
+           if (kill_advmame) {
+             pressButton(SDL_CONTROLLER_BUTTON_LEFTSTICK);
+             SDL_Delay(100);
+             pressButton(SDL_CONTROLLER_BUTTON_DPAD_UP);
+             SDL_Delay(100);
+             pressButton(SDL_CONTROLLER_BUTTON_B);
+             SDL_Delay(1000);
+             exit(0);
+           }
           if (pckill_mode) {
             emitKey(KEY_F4,true,KEY_LEFTALT);
             SDL_Delay(15);
@@ -2341,6 +2371,8 @@ int main(int argc, char* argv[])
       if (ii + 1 < argc) { 
         kill_mode = true;
         AppToKill = argv[++ii];
+        if (strcmp(AppToKill, "advmame") == 0)
+          kill_advmame = true;
       }
     } else if ((strcmp(argv[ii], "-sudokill") == 0)) {
       if (ii + 1 < argc) { 
